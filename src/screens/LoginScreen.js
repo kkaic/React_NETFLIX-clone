f652c56162b56d4f0875fb101fa1abd71baad107
kkaic <kkchuchang@gmail.com>
2021-09-23 22:43:28 +0800

    38mins

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./LoginScreen.css";
+
+
+function LoginScreen() {
+  return (
+    <div className="loginScreen">
+      <div className="loginScreen_background">
+        <img
+          className="loginScreen_logo"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoHvd3HruAm79Dj-aJB2kqNT4IfHOzNBzmXbuTu7X4X78xh7nduzl29uOVc3xIRooOSKc&usqp=CAU"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+  
+}
+
+export default LoginScreen;
